1ade405e02ee6bf66d7edd476f9b422a8dd604e3
Ubuntu <ubuntu@ip-172-31-2-27.ap-southeast-2.compute.internal>
2014-05-31 14:58:58 +0000

    init

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var fs = require('fs');
-var htmlfile = "index.html";
-
+// web.js
+var express = require("express");
+var logfmt = require("logfmt");
 var app = express();
 
-app.get('/', function(request, response){
-    var html = fs.readFileSync(htmlfile).toString();
-    response.send(html);
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
 
-var port = process.env.PORT || 8080;
-app.listen(port, function(){
-    console.log("Listening on " +port);
+var port = Number(process.env.PORT || 8080);
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });

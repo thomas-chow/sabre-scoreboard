9bbce6ca2b6af4ce3dbd3dd56b3218aa2f87e56e
thomas-chow <tykchow@Thomas-laptop.(none)>
2014-05-31 23:08:43 +1000

    first upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
+
+var app = express();
+
+app.use("/css", express.static(__dirname + '/css'));
+
+app.get('/', function(request, response){
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function(){
+    console.log("Listening on " +port);
+});
+
